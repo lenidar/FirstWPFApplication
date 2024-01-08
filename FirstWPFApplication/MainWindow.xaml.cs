a63530313bb77f67e1f551511d36ea10462230c8
lenidar <55443074+lenidar@users.noreply.github.com>
2024-01-08 17:34:05 +0800

    Add project files.

---
+++ FirstWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClickMe_MouseEnter(object sender, MouseEventArgs e)
+        {
+            float left = rnd.Next(0, (int)myWindow.Width - (int)btnClickMe.Width);
+            float top = rnd.Next(0, (int)myWindow.Height - (int)btnClickMe.Height);
+            //MessageBox.Show("Mouse Hover");
+
+            btnClickMe.Margin = new Thickness(left, top, 0, 0);
+
+        }
+    }
+}
---
+++ FirstWPFApplication/Window2.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        string name = "";
+        public Window2()
+        {
+            InitializeComponent();
+
+            txtInput.Text = "What is your name?";
+            name = "";
+            btnGetInput.IsEnabled = false;
+        }
+
+        private void txtInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Focused");
+            if(name.Length == 0)
+            {
+                txtInput.Text = "";
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //MessageBox.Show("Text Change trigger");
+            name = txtInput.Text;
+
+            if(name.Length > 0)
+                btnGetInput.IsEnabled = true;
+            else
+                btnGetInput.IsEnabled = false;
+        }
+
+        private void btnGetInput_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello there {name}!");
+        }
+    }
+}
